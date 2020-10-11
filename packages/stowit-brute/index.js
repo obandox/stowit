e87127ffb4d2108f46ba9d49dce44e8e9597475f
Miguel Obando <miguel@checkapp.com>
2020-10-11 02:25:35 -0500

    feat(stowit-brute): add commander to stowit brute base

--- packages/stowit-brute/index.js
+++ packages/stowit-brute/index.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 const axios = require("axios");
 const fileStorage = require("./fileStorage");
-
+const { program } = require("commander");
 const BRUTE_TAG = "$brute";
 const ENGLISH_LETTERS = "abcdefghijklmnopqrstuvwxyz";
 const ARABIC_NUMBERS = "0123456789";
@@ -17,6 +19,8 @@ const indexToString = ({ index, charset }) => {
 };
 
 const formatFolderName = (text) => text.replace(/[^\w\-]+/g, "_");
+const sleep = (seconds = 0.2) =>
+  new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 
 const brute = async (pattern) => {
   const parts = pattern.split(BRUTE_TAG);
@@ -26,29 +30,43 @@ const brute = async (pattern) => {
   let currentIndex = 0;
   const patternFolder = formatFolderName(parts[0]);
 
-  while (currentIndex <= 300) {
+  while (currentIndex <= Number.MAX_SAFE_INTEGER) {
     const word = indexToString({ index: currentIndex, charset: characters });
-    currentIndex += 1;
 
-    const url = new URL(parts[0] + word + parts[1]);
+    const url = new URL(parts[0] + word + (parts[1] || ""));
     const fileName = `${formatFolderName(url.href)
       .replace(parts[0], "")
       .replace(parts[1], "")}`;
 
     const htmlFilepath = `${patternFolder}/html/${fileName}.html`;
     if (await fileStorage.hasItem(htmlFilepath)) {
+      currentIndex += 1;
       continue;
     }
-    const response = await axios.get(url.href);
+    const response = await axios.get(url.href).catch((error) => error.response);
+    if (response.status === 429) {
+      console.log("TOO MANY REQUEST AT " + url.href);
+      await sleep(120);
+    }
+    if (response.status >= 500) {
+      console.log("ERROR WITH " + url.href);
+      await sleep(10);
+      continue;
+    }
     await fileStorage.setItem(htmlFilepath, response.data.toString());
     const statusFilepath = `${patternFolder}/${response.status}/${fileName}.json`;
     await fileStorage.setJson(statusFilepath, {
       href: url.href,
       status: response.status,
       statusTexT: response.statusText,
+      headers: response.headers,
     });
     console.log("resolved: ", url.href);
+    currentIndex += 1;
   }
 };
 
-brute("https://www.npmjs.com/package/$brute").catch(console.error);
+program
+  .arguments("<pattern>")
+  .action((pattern) => brute(pattern).catch(console.error));
+program.parse(process.argv);
